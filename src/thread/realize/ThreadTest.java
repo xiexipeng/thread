127152f421ed1dffe06ecbac2a6c39230804b75b
xiexipeng <1368010958@qq.com>
2017-05-29 23:56:19 +0800

    xxp

--- src/thread/realize/ThreadTest.java
+++ src/thread/realize/ThreadTest.java
@@ -12,15 +12,19 @@ public class ThreadTest {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		System.out.println("主线程开始执行...............");
+		//多线程实现方式1
 		BasicThread1 bt1 = new BasicThread1();
 		bt1.start();
+		//多线程实现方式2
 		BasicThread2 bt2 = new BasicThread2();
 		Thread t = new Thread(bt2);
 		t.start();
+		//多线程实现方式3
 		BasicThread3 bt3 = new BasicThread3();
 		FutureTask<Object> task = new FutureTask<Object>(bt3);
 		Thread t2 = new Thread(task);
 		t2.start();
+		//多线程实现方式4
 		ExecutorService es = Executors.newCachedThreadPool();
 		BasicThread4 bt4 = new BasicThread4();
 		Future<Object> future = es.submit(bt4);
@@ -37,6 +41,7 @@ public class ThreadTest {
 
 }
 
+//继承Thread类实现多线程
 class BasicThread1 extends Thread {
 
 	@Override
@@ -51,6 +56,7 @@ class BasicThread1 extends Thread {
 	}
 }
 
+//实现Runnable接口实现多线程
 class BasicThread2 implements Runnable {
 
 	@Override
@@ -66,13 +72,14 @@ class BasicThread2 implements Runnable {
 
 }
 
+//实现Callbale接口实现多线程
 class BasicThread3 implements Callable<Object> {
 
 	@Override
 	public Object call() throws Exception {
 		try {
 			long start = System.currentTimeMillis();
-			Thread.sleep(5000);
+			Thread.sleep(4000);
 			System.out.println("线程3执行完毕,总耗时：" + (System.currentTimeMillis() - start));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -82,13 +89,14 @@ class BasicThread3 implements Callable<Object> {
 
 }
 
+//实现Callable并通过线程池实现多线程
 class BasicThread4 implements Callable<Object> {
 
 	@Override
 	public Object call() throws Exception {
 		try {
 			long start = System.currentTimeMillis();
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 			System.out.println("线程4执行完毕,总耗时：" + (System.currentTimeMillis() - start));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
