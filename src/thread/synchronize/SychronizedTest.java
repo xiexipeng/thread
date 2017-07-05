e1f78032335fbfdca046e78c1dcb0c0186742874
xiexipeng <1368010958@qq.com>
2017-07-05 22:12:50 +0800

    添加基于类的同步代码块

--- src/thread/synchronize/SychronizedTest.java
+++ src/thread/synchronize/SychronizedTest.java
@@ -3,7 +3,7 @@ package thread.synchronize;
 public class SychronizedTest {
 
 	public void syctest() {
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.println("线程" + Thread.currentThread().getName() + "正在执行");
 			try {
 				Thread.sleep(500);
@@ -14,7 +14,7 @@ public class SychronizedTest {
 	}
 
 	public synchronized void sycIntest() {
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.println("线程" + Thread.currentThread().getName() + "正在执行");
 			try {
 				Thread.sleep(500);
@@ -25,7 +25,7 @@ public class SychronizedTest {
 	}
 
 	public synchronized void sycIntest2() {
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.println("线程" + Thread.currentThread().getName() + "正在执行");
 			try {
 				Thread.sleep(500);
@@ -37,7 +37,20 @@ public class SychronizedTest {
 
 	public void syctest2() {
 		synchronized (this) {
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("线程" + Thread.currentThread().getName() + "正在执行");
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void syctest3() {
+		synchronized (SychronizedTest.class) {// 类锁。注意，这表示该类所有对象实例同时只能有一个访问该代码块。
+			for (int i = 0; i < 3; i++) {
 				System.out.println("线程" + Thread.currentThread().getName() + "正在执行");
 				try {
 					Thread.sleep(500);
@@ -108,6 +121,23 @@ public class SychronizedTest {
 		t2.start();
 	}
 
+	public void runTest5() {
+		SychronizedTest st1 = new SychronizedTest();
+		SychronizedTest st2 = new SychronizedTest();
+		Thread t1 = new Thread("任务1") {
+			public void run() {
+				st1.syctest3();
+			}
+		};
+		Thread t2 = new Thread("任务2") {
+			public void run() {
+				st2.syctest3();
+			}
+		};
+		t1.start();
+		t2.start();
+	}
+
 	public static void main(String[] args) {
 		SychronizedTest st = new SychronizedTest();
 		// 不同线程对同一对象的同一个同步方法的访问
@@ -117,7 +147,9 @@ public class SychronizedTest {
 		// 不同线程对同一兑现的两个不同的同步方法的访问
 		// st.runTest3();
 		// 不同线程对同一对象的一个同步方法和同步代码块的访问
-		st.runTest4();
+		// st.runTest4();
+		// 不同线程对同一对象的一个同步方法和同步代码块的访问
+		st.runTest5();
 	}
 
 }
